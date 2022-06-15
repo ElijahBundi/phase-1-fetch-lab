1c593428ee9fe368691c1e23467d50e797dbe447
Elijah <bundielly@gmail.com>
2022-06-15 06:09:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then ((result) => {
+    return result.json()
+  })
+  .then((jsonData) => {
+    return renderBooks(jsonData)
+  })
 }
 
 function renderBooks(books) {
